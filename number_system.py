6a2e4d21fb22902b107192f4085ea46d464855eb
biruk60 <biruk60@users.noreply.github.com>
2025-09-16 16:11:32 -0400

    Update number_system.py

--- number_system.py
+++ number_system.py
@@ -1,6 +1,78 @@
 from manim import *
 from manim.mobject.geometry.tips import StealthTip
 
+class IntroScene(Scene):
+    def construct(self):
+        # Define the number line
+        triangle = Triangle()
+        dot = Dot(3 * UR, color=GREEN)
+        circle = Circle()
+        square = Square()
+        self.play(SpinInFromNothing(triangle))
+        self.play(GrowFromPoint(circle, dot, dot.get_color()))
+        self.play(GrowFromPoint(square, ORIGIN))
+        shapes = VGroup(triangle, circle, square)
+        self.play(ShrinkToCenter(shapes))
+        # self.play(ShrinkToCenter(triangle),ShrinkToCenter(circle),ShrinkToCenter(square))
+        
+        # self.play(FadeTransform(triangle, circle))
+        self.wait(1)
+        ax = Axes()
+        sine = ax.plot(np.sin, color=RED)
+        alpha = ValueTracker(0)
+        point = always_redraw(
+            lambda: Dot(
+                sine.point_from_proportion(alpha.get_value()),
+                color=BLUE
+            )
+        )
+        tangent = always_redraw(
+            lambda: TangentLine(
+                sine,
+                alpha=alpha.get_value(),
+                color=YELLOW,
+                length=4
+            )
+        )
+        self.add(ax, sine, point, tangent)
+        self.play(alpha.animate.set_value(1), rate_func=linear, run_time=2)
+        graphs = VGroup(ax, sine, point, tangent)
+      
+        colors = [RED, GREEN, BLUE]
+
+        starting_points = VGroup(
+            *[
+                Dot(LEFT + pos, color=color)
+                for pos, color in zip([UP, DOWN, LEFT], colors)
+            ]
+        )
+        self.add(starting_points)
+        self.play(Transform(graphs,starting_points))
+
+        finish_points = VGroup(
+            *[
+                Dot(RIGHT + pos, color=color)
+                for pos, color in zip([ORIGIN, UP, DOWN], colors)
+            ]
+        )
+
+        
+        
+        for dot in starting_points:
+            self.add(TracedPath(dot.get_center, stroke_color=dot.get_color()))
+
+        self.wait()
+        self.play(
+            Create(finish_points),
+            Transform(
+                starting_points,
+                finish_points,
+                path_func=utils.paths.clockwise_path(),
+                run_time=2,
+            )
+        )
+        self.wait()
+        
 class NumberLineWithAnnotations(Scene):
     def construct(self):
         # Define the number line
